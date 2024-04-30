97f6d557acc8a30117e384f10e3960ffbbd0c4be
mwilco03 <36248105+mwilco03@users.noreply.github.com>
2024-04-30 08:39:29 -0400

    Update rando.sh

--- Scripts/rando.sh
+++ Scripts/rando.sh
@@ -3,7 +3,6 @@
 #      $1 :   number (of characters)
 # returns :   string of random characters
 
-# rando() { shuf -en $1 {{a..z},{0..9},{A..Z}} | tr -d $'\n' ; }
-# export -f rando
-
-shuf -en $1 {{a..z},{0..9},{A..Z}} | tr -d $'\n'
+rando() { shuf -en $1 {{a..z},{0..9},{A..Z}} | tr -d $'\n' ; }
+export -f rando
+# shuf -en $1 {{a..z},{0..9},{A..Z}} | tr -d $'\n'
