2a61739cc5d7abc931c1e236081a5626594a2e4f
Zach Pendleton <154548075+asmrtfm@users.noreply.github.com>
2024-05-02 14:07:41 -0400

    Update rando.sh

--- Scripts/rando.sh
+++ Scripts/rando.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+## to use in the current session, simply define a function:
+#rando() { shuf -en $1 {{a..z},{0..9},{A..Z}} | tr -d '\n' ; }
+
+## if adding to an rc file, such as ~/.bash_aliases, don't forget to export the function:
+#export -f rando
+
+## otherwise, just use this script as is.
 #      $1 :   number (of characters)
 # returns :   string of random characters
-
-rando() { shuf -en $1 {{a..z},{0..9},{A..Z}} | tr -d $'\n' ; }
-export -f rando 
-# shuf -en $1 {{a..z},{0..9},{A..Z}} | tr -d $'\n'
-# shuf -en $1 {{a..z},{0..9},{A..Z}} | tr -d $'\n'
+shuf -en $1 {{a..z},{0..9},{A..Z}} | tr -d '\n'
